9d5a8c84f4d0049890a8444bbfe171db01d051fa
MessiHL <roadsclub@163.com>
2022-04-08 10:52:41 +0800

    feat:环境搭建

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# front-end-learning-vue3
-vue3 学习
+# front-end-learning-vue3 Vue 3 + Typescript + Vite 
+
+## 参考资料
+
+[Vue3.js 官网](https://vuejs.org/)
+[Vite 官方中文文档](https://cn.vitejs.dev/guide/)
+
+## 环境搭建与配置
+
+```
+npm create vite@latest my-vue-app -- --template vue-ts
+```
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h2>vue3</h2>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
