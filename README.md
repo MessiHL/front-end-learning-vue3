e76559fe2d9e2d56cb322686692e7f2a9ec16c4e
MessiHL <roadsclub@163.com>
2022-04-21 08:53:05 +0800

    项目重构

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
-# front-end-learning-vue3 Vue 3 + Typescript + Vite 
-
-## 参考资料
-
-[Vue3.js 官网](https://vuejs.org/)
-
-[Vite 官方中文文档](https://cn.vitejs.dev/guide/)
-
-[Vue3.0学习笔记【掘金】](https://juejin.cn/post/6908185323801575432)
-
-[浅谈前端路由原理hash和history](https://juejin.cn/post/6993840419041706014)
+# Vue 3 + TypeScript + Vite
 
 
-## 环境搭建与配置
-
-```
-npm create vite@latest front-end-learning-vue3 -- --template vue-ts
-```
-
-## 学习：
-入门：
-- [官网tutorial](https://vuejs.org/tutorial/#step-1)
-- [官网examples](https://vuejs.org/examples/)
+## 参考 
+ - [Vue3 + vite + Ts + pinia + 实战 + 源码 +全栈](https://www.bilibili.com/video/BV1dS4y1y7vd?p=3&spm_id_from=pageDriver)
+ - [CSDN-小满-学习Vue3](https://blog.csdn.net/qq1195566313/article/details/122768533?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522165050174016780366543922%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=165050174016780366543922&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-122768533.142^v9^pc_search_result_control_group,157^v4^control&utm_term=%E5%B0%8F%E6%BB%A1&spm=1018.2226.3001.4187)
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
-<script lang="ts">
-import TreeItem from './examples/TreeView/TreeItem.vue'
-import { defineComponent, ref} from 'vue'
-export default defineComponent({
-  name: 'App',
-  components:{
-    TreeItem
-  },
-  setup(props) {
-    const treeData = ref({
-    name: 'My Tree',
-    children: [
-      { name: 'hello' },
-      { name: 'wat' },
-      {
-        name: 'child folder',
-        children: [
-          {
-            name: 'child folder',
-            children: [{ name: 'hello' }, { name: 'wat' }]
-          },
-          { name: 'hello' },
-          { name: 'wat' },
-          {
-            name: 'child folder',
-            children: [{ name: 'hello' }, { name: 'wat' }]
-          }
-        ]
-      }
-    ]
-  })
-    
-    return {
-      treeData
-    }
-  }
-})
-
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-   <ul>
-     <TreeItem class="item" :model="treeData">{{treeData.name}}</TreeItem>
-   </ul>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
